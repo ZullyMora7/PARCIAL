2c20fcf80eff61c19c09b1add814deed39b1962f
zully <zullymoraflores@gmail.com>
2025-09-09 08:15:46 -0500

    finalizacion del parcial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function mostrarConsola(){
+    console.log("Usted busco:" + buscar);
+}
+
+function mostrarEnConsola() {
+    console.log("Su usuario es : "+ usuario.value + " Su contrasena es : " + contraseña.value);
+}
+
+function cambiaTamanoFuente() {
+    document.body.style.fontSize = "25px";
+}
+
+
+function cambiarColoLetra() {
+    document.body.querySelector("p").color = "blue";}
+
+  
